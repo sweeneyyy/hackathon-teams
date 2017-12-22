7f4bfc9350594cffa204e576e9dc1f4101ecc2e5
sweeneyyy <sweeneyarnett@gmail.com>
2017-12-21 18:51:24 -0800

    delete working

--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -12,4 +12,18 @@ $('.delete').click(function(e){
   });
 });
 
+
+// $('.edit').click(function(e){
+//   e.preventDefault();
+//   // ajax call that automatically uses method POST
+//   $.ajax({
+//     url: $(this).attr('href'),
+//     method: 'DELETE'
+//     //if return successfully
+//   }).done(function(data){
+//     window.location.href = '/teams';
+//   });
+// });
+
+
 });
