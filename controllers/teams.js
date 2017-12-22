8f922a881ee28546bd592d6923dd7fbf1d77a394
sweeneyyy <sweeneyarnett@gmail.com>
2017-12-21 18:25:53 -0800

    delete team working

--- controllers/teams.js
+++ controllers/teams.js
@@ -14,6 +14,18 @@ router.post('/', function(req, res) {
   res.redirect('/teams');
 });
 
+
+router.delete('/:name', function(req, res){
+  // console.log('Delete team name: ', req.params.name);
+  teamService.deleteTeam(req.params.name);
+  res.send('Success');
+  // }).catch(function(err){
+  //   console.log('An error happened', err);
+  //   res.send('fail');
+  // });
+});
+
+
 router.get('/new', function(req, res) {
   res.render('teams/new');
 });
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+$('.delete').click(function(e){
+  e.preventDefault();
+  // ajax call that automatically uses method POST
+  $.ajax({
+    url: $(this).attr('href'),
+    method: 'DELETE'
+    //if return successfully
+  }).done(function(data){
+    window.location.href = '/teams';
+  });
+});
+
 });
